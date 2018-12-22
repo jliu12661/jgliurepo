0c8f41705bc3860d8854eb27a1c937e34ee97ac0
ccliu <charlie.liu@thermofiosher.com>
2018-12-21 21:16:33 -0800

    Remove duplicated

--- calculators/calculators/Form1.cs
+++ calculators/calculators/Form1.cs
@@ -19,34 +19,48 @@ namespace calculators
 
         private void button_add_Click(object sender, EventArgs e)
         {
-            double input_1 = Double.Parse(textBox_input_1.Text);
-            double input_2 = Double.Parse(textBox_input_2.Text);
-            double results = input_1 + input_2;
-            textBox_results.Text = results.ToString();
+            Calculate("Add");
         }
 
         private void button_subtract_Click(object sender, EventArgs e)
         {
-            double input_1 = Double.Parse(textBox_input_1.Text);
-            double input_2 = Double.Parse(textBox_input_2.Text);
-            double results = input_1 - input_2;
-            textBox_results.Text = results.ToString();
+            Calculate("Subtract");
         }
 
         private void button_multiply_Click(object sender, EventArgs e)
         {
-            double input_1 = Double.Parse(textBox_input_1.Text);
-            double input_2 = Double.Parse(textBox_input_2.Text);
-            double results = input_1 * input_2;
-            textBox_results.Text = results.ToString();
+            Calculate("Multiply");
         }
 
         private void button_divide_Click(object sender, EventArgs e)
         {
+            Calculate("Divide");
+        }
+
+        private void Calculate(string op)
+        {
             double input_1 = Double.Parse(textBox_input_1.Text);
             double input_2 = Double.Parse(textBox_input_2.Text);
-            double results = input_1 / input_2;
+
+            double results = 0;
+            switch(op)
+            {
+                case "Add":
+                    results = input_1 + input_2;
+                    break;
+                case "Subtract":
+                    results = input_1 - input_2;
+                    break;
+                case "Multiply":
+                    results = input_1 * input_2;
+                    break;
+                case "Divide":
+                    results = input_1 / input_2;
+                    break;
+            }
+
             textBox_results.Text = results.ToString();
         }
+
     }
 }
